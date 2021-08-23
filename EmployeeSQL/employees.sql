95508e0fbe4dbbedaf990bb2e73a023baddf9db9
Nathan Haile <hailenathan0@gmail.com>
2021-08-23 12:21:05 -0400

    final

--- EmployeeSQL/employees.sql
+++ EmployeeSQL/employees.sql
@@ -60,7 +60,7 @@ REFERENCES "titles" ("title_id");
 ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
-
+--View and verify data
 
 SELECT * FROM employees;
 SELECT * FROM departments;
@@ -69,6 +69,7 @@ SELECT * FROM salaries;
 SELECT * FROM titles;
 SELECT * FROM dept_manager;
 
+--1.
 
 CREATE VIEW emp_salaries AS
 SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
@@ -76,12 +77,13 @@ FROM employees
 INNER JOIN salaries ON
 employees.emp_no = salaries.emp_no;
 
-
+--2.
 CREATE VIEW emp_1986 AS
 SELECT first_name, last_name, hire_date 
 FROM employees
 WHERE hire_date BETWEEN '1986-01-01' AND '1987-01-01';
 
+--3.
 CREATE VIEW managers_dept AS 
 SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
 FROM departments
@@ -90,6 +92,7 @@ departments.dept_no=dept_manager.dept_no
 INNER JOIN employees ON
 dept_manager.emp_no=employees.emp_no;
 
+--4.
 CREATE VIEW emp_departments AS
 SELECT dept_emp.emp_no, employees.last_name,employees.first_name,departments.dept_name
 FROM dept_emp
@@ -98,12 +101,13 @@ dept_emp.emp_no=employees.emp_no
 INNER JOIN departments ON
 dept_emp.dept_no = departments.dept_no;
 
-
+--5.
 CREATE VIEW herc_emp AS
 SELECT first_name, last_name, sex
 FROM employees
 WHERE first_name='Hercules'AND last_name LIKE 'B%';
 
+--6.
 CREATE VIEW sales_emp AS
 SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
 FROM dept_emp
@@ -113,7 +117,7 @@ INNER JOIN departments ON
 dept_emp.dept_no=departments.dept_no
 WHERE departments.dept_name ='Sales';
 
-
+--7.
 CREATE VIEW last_name_count AS
 SELECT last_name,
 COUNT(last_name) 
@@ -122,6 +126,21 @@ GROUP BY last_name
 ORDER BY
 COUNT(last_name) DESC;
 
+--Export Views
+
+COPY (SELECT * FROM emp_1986) TO 'C:\Users\Public\emp_data1986.csv' DELIMITER ',' CSV HEADER;
+
+COPY (SELECT * FROM emp_departments) TO 'C:\Users\Public\emp_departments.csv' DELIMITER ',' CSV HEADER;
+
+COPY (SELECT * FROM emp_salaries) TO 'C:\Users\Public\emp_salaries.csv' DELIMITER ',' CSV HEADER;
+
+COPY (SELECT * FROM herc_emp) TO 'C:\Users\Public\herc_emp.csv' DELIMITER ',' CSV HEADER;
+
+COPY (SELECT * FROM last_name_count) TO 'C:\Users\Public\last_name_count.csv' DELIMITER ',' CSV HEADER;
+
+COPY (SELECT * FROM managers_dept) TO 'C:\Users\Public\managers_dept.csv' DELIMITER ',' CSV HEADER;
+
+COPY (SELECT * FROM sales_emp) TO 'C:\Users\Public\sales_emp.csv' DELIMITER ',' CSV HEADER;
 
 
 
